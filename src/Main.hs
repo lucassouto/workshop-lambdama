9693cd7da100db517fd68f3d4739c467d98e43c7
Lucas Souto <lucasinfologos@gmail.com>
2018-04-21 10:11:25 -0300

    Data types

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,9 @@
 module Main where
 
+data DiaSemana  = Segunda | Terca | Quarta | Quinta | Sexta | Sabado | Domingo
+                  deriving Show
+
 main :: IO ()
 main = do
   putStrLn "hello world"
+  print Segunda
